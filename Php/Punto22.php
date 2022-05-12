00c8b4c6e1257965c75ca2c7f1548a086d90a143
andres Londoño <andresmedellin215@gmail.com>
2022-05-12 11:23:40 -0500

    punto 22 y 23 estan  funcionales

--- Php/Punto22.php
+++ Php/Punto22.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Punto 22</title>
 </head>
 <body>
+    <h1>Bienvenidos al ejercicio N22#</h1>
+    <p>    
+    Se tienen tres baldes de agua, uno de cinco litros, otros de tres litros y otro de un litro; <br>
+    si el de un litro tarda una hora y media en llenarse, resuelva cuanto tiempo pueden tardar en llenarse los otros baldes. <br>
+    Si tiene tres baldes, pero se desconoce su tamaño debe de resolver igualmente el ejercicio. <br>
+
+
+    </p>
+
+  
+     <br><br>
     
+        <h3>balde # 1 se demora  : 7 h y 30 min en llenar</h3>
+        <h3>balde # 2 se demora  : 4 h y 30  min en llenar</h3>
+        <h3>balde # 3 se demora  : 1 y 30 min   en llenar</h3>
+    <a href="../Index.html" class="btn btn-danger">Volver</a> <br><br>
 </body>
 </html>
--- Php/Punto23.php
+++ Php/Punto23.php
@@ -4,9 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Punto 23</title>
 </head>
 <body>
-    
+    <h1>Bienvenidos al ejercicio N23#</h1>
+    <p>    
+    Una persona tarda 5 horas en subir una montaña de 7 metros, <br>
+     si un escalador desea subir más o menos de la montaña, <br>
+      cuanto tiempo tarda en subir. Debe de resolver el ejercicio.  <br>
+
+
+
+    </p>
+
+    <div>
+    <form  name="punto23" action="../Php/Punto23.php" method="POST">
+
+
+        
+        <input type="number" name="altura" id="" placeholder="Ingrese altura" min="1" >
+      
+        <br><br><br>
+        
+        <button type="submit" class="btn btn-primary" value="calcular" name="calcular">Calcular</button>
+     
+     
+      
+    </form>
+    </div>
+    <?php 
+      
+        $tiempo=5;
+        $altura2=7;
+         if(isset($_POST['calcular'])){
+            
+           
+            $altura = $_POST["altura"];
+
+            $resultado=round(($altura*$tiempo)/$altura2,2);
+            echo "lo que se demora es ".$resultado. "Horas";
+        }
+        
+    ?>
+     <br><br>
+
+    <a href="../Index.html" class="btn btn-danger">Volver</a> <br><br>
 </body>
 </html>
